96f80c84efd5f8809c47efaaa3ae518a1b84ec0a
swapnil <swapnilg683@gmail.com>
2022-06-06 23:56:12 +0530

    :hammer: small comment mistake :hammer:

--- Searching-Algo/binary-search.js
+++ Searching-Algo/binary-search.js
@@ -3,7 +3,6 @@ Swapnil Gopale
 Name: Binary Search
 Worst Case time complexity O(log(n))
 Best Case time complexity O(1)
-No need of sorted array
 
 Function will take an sorted array as a first parameter and value to be search as second parameter.
 Return index where it first occured in array
